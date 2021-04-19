c4175bacc6c39237ed5185b8116f9f16d43416d1
Nikolas9898 <nikolanikolchov@gmail.com>
2021-04-19 17:28:00 +0300

    fixed small warning

--- src/pages/transactionPage/MonthlyPage.tsx
+++ src/pages/transactionPage/MonthlyPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import queryString from 'query-string';
+import {RouteComponentProps} from 'react-router';
 import NavBar from '../../layout/header/Header';
 import MonthlyContainer from '../../modules/transaction/monthlyContainer/MonthlyContainer';
 
-const MonthlyPage = (props: any): JSX.Element => {
+const MonthlyPage = (props: RouteComponentProps): JSX.Element => {
   const {location} = props;
   const filters = queryString.parse(location.search);
   return (
