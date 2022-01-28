f8b7c51b4b70e9466eb64bfab100422f4640bb4e
Nikola9898 <nikolanikolchov@gmail.com>
2022-01-29 00:20:17 +0200

    dev thing

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 // Next we make an 'instance' of it
 const instance = axios.create({
   // .. where we make our configurations
-  baseURL: 'http://localhost:5000'
+  baseURL: ''
 });
 
 // Where you would set stuff like your 'Authorization' header, etc ... TODO
